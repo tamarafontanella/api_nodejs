d03e57f7966e696b251fb610c3275c48b9d714d5
tamara.lima <tamara.lima@br.experian.com>
2023-03-19 19:11:22 -0300

    studiyng javascript api

---
+++ 04-lists/for-forin/1.for-forin-forof.js
@@ -0,0 +1,19 @@
+const service = require('./service')
+
+async function main() {
+    try {
+        const result = await service.obterPessoas('a')
+        const names = []
+        console.time('for')
+        for (let i = 0; i <= result.results.length - 1; i++) {
+            const pessoa = result.results[i]
+            names.push(pessoa.name)
+        }
+        console.timeEnd('for')
+        console.log(`names`, names)
+    } catch (error) {
+        console.error(`erro interno`, error)
+    }
+}
+
+main()
